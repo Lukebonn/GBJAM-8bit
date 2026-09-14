3495506fa65d0a6b689e3903a187a70438b0a1b4
Luke Bonniwell <113559516+Lukebonn@users.noreply.github.com>
2026-09-14 19:22:59 -0400

    crafting + pause menus functionality increase

--- Scripts/Potions/potion_actual.gd
+++ Scripts/Potions/potion_actual.gd
@@ -1,18 +1,29 @@
 extends Node2D
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
-@export var potion_stats = Resource
+@export var potion_stats = PotionResource
+@onready var label: Label = $Label
+
+var item_quantity
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	#pass
 	sprite_2d.texture = potion_stats.sprite
 	print(potion_stats.rarity)
+	
+	if item_quantity == 1:
+		label.visible = false
+	else: 
+		label.visible = true
 
+func increase_item_quantity(amount_to_add):
+	item_quantity += amount_to_add
+	label.text = String(item_quantity)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func decrease_item_quantity(amount_to_remove):
+	item_quantity += amount_to_remove
+	label.text = String(item_quantity)
 
 func set_position_in_slot(slot_size: Vector2) -> void:
 	sprite_2d.position = slot_size / 2
