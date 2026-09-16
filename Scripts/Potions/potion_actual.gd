20b80e58fff0be0a32088540f0e881c962ac2bb7
Luke Bonniwell <113559516+Lukebonn@users.noreply.github.com>
2026-09-16 00:25:38 -0400

    crafting UI basic setup

--- Scripts/Potions/potion_actual.gd
+++ Scripts/Potions/potion_actual.gd
@@ -12,10 +12,10 @@ func _ready() -> void:
 	sprite_2d.texture = potion_stats.sprite
 	print(potion_stats.rarity)
 	
-	if item_quantity == 1:
-		label.visible = false
-	else: 
-		label.visible = true
+	#if item_quantity == 1:
+		#label.visible = false
+	#else: 
+		#label.visible = true
 
 func increase_item_quantity(amount_to_add):
 	item_quantity += amount_to_add
@@ -26,4 +26,4 @@ func decrease_item_quantity(amount_to_remove):
 	label.text = String(item_quantity)
 
 func set_position_in_slot(slot_size: Vector2) -> void:
-	sprite_2d.position = slot_size / 2
+	sprite_2d.position = slot_size
