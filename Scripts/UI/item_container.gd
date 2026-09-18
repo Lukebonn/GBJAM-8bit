f9db7d1054109f700119ad21c4d3760ccbdcf2b0
Jack Lynch <jrl31904@gmail.com>
2026-09-17 21:26:10 -0400

    Start UI setup

---
+++ Scripts/UI/item_container.gd
@@ -0,0 +1,15 @@
+class_name ItemContainer
+extends TextureRect
+
+@export var item : Resource
+
+signal selected(slot : String)
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
--- Scripts/UI/pause_menu.gd
+++ Scripts/UI/pause_menu.gd
@@ -9,6 +9,7 @@ signal switch_tab(tab : int)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	#visible = false
 	set_cur_tab(current_tab)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
