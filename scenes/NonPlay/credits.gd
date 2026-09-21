0abbb57dd83d52587aaa352a4a6fd8a5c9d8b046
Luke Bonniwell <113559516+Lukebonn@users.noreply.github.com>
2026-09-21 05:12:50 -0400

    testing recipes

--- scenes/NonPlay/credits.gd
+++ scenes/NonPlay/credits.gd
@@ -7,6 +7,6 @@ func _ready() -> void:
 	await get_tree().create_timer(2.5).timeout
 	just_joined = true
 
-func _process(delta: float) -> void:
-	if Input.is_anything_pressed() and just_joined:
-		NavigationManager.go_to_level("kitchen", "W")
+#func _process(delta: float) -> void:
+	#if Input.is_anything_pressed() and just_joined:
+		#NavigationManager.go_to_level("kitchen", "W")
--- scenes/kitchen.gd
+++ scenes/kitchen.gd
@@ -1,8 +1,9 @@
 extends Node2D
 
 @onready var theme: AudioStreamPlayer2D = %theme
-
+#@onready var sub_viewport: SubViewport = $SubViewport
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	theme.play()
+	#sub_viewport.position = Vector2(200, 150)
